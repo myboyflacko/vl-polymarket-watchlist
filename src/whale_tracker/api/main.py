8e049c0723c800beffefcbae74a44969bb208108
felix <felix.r.jenisch@gmail.com>
2026-06-03 20:39:14 +0200

    whales endpoint added

--- src/whale_tracker/api/main.py
+++ src/whale_tracker/api/main.py
@@ -1,19 +1,21 @@
 from fastapi import FastAPI
 
-from whale_tracker.tracker.markets.repository import get_latest_market_run_id, list_markets
-
+from whale_tracker.tracker.markets.repository import get_latest_market_run_id, list_qualified_markets
+from whale_tracker.tracker.whales.repository import get_latest_discovery_run_id, list_discovered_whales
 
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
 @app.get("/markets")
 def get_latest_markets():
     run_id = get_latest_market_run_id()
-    data = list_markets(run_id=run_id)
-    return data
+    markets = list_qualified_markets(run_id=run_id)
+    return markets
+
+
+@app.get("/whales")
+def get_latest_whales():
+    run_id = get_latest_discovery_run_id()
+    whales = list_discovered_whales(run_id=run_id)
+    return whales
